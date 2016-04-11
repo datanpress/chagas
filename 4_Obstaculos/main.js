58a09b94393d1c33bf4f04b77f8f5c082e4c7785
David Mabodo <david.mabodo@gmail.com>
2016-04-11 23:31:06 +0200

    improves on 4

--- 4_Obstaculos/main.js
+++ 4_Obstaculos/main.js
@@ -16,7 +16,28 @@ d3.xml("4.svg", "image/svg+xml", function(error, xml) {
   document.body.appendChild(xml.documentElement);
   //translate
   
-  d3.select('#title').text(iJS._("Eficacia del tratamiento antiparasitario"));
+  d3.select('#title').text(iJS._("Obstáculos para frenar la transmisión"));
+  d3.select('#centraltext').text(iJS._("FALTA DE DIAGNÓSTICO"));
+  d3.select('#vectortext1').text(iJS._("Picadura"));
+  d3.select('#vectortext2').text(iJS._("Contaminación"));
+  d3.select('#vectortext3').text(iJS._("DE BEBIDAS O ALIMENTOS"));
+  d3.select('#wtitle1').text(iJS._("TRANSMISIÓN"));
+  d3.select('#wtitle2').text(iJS._("NO VECTORIAL"));
+  d3.select('#mtitle1').text(iJS._("TRANSMISIÓN"));
+  d3.select('#mtitle2').text(iJS._("POR VECTOR"));
+  d3.select('#mcount1').text(iJS._("PAÍSES ENDEMICOS"));
+  d3.select('#mcount2').text(iJS._("PAÍSES ENDÉMICOS Y NO ENDÉMICOS"));
+  d3.select('#rtext1').text(iJS._("EMBARAZO"));
+  d3.select('#rtext2').text(iJS._("SIN TRATAMIENTO PREVIO"));
+  d3.select('#rtext3').text(iJS._("DONACIÓN DE SANGRE"));
+  d3.select('#rtext4').text(iJS._("SIN CRIBAR"));
+  d3.select('#rtext5').text(iJS._("DONACIÓN DE ÓRGANOS"));
+  d3.select('#rtext6').text(iJS._("SIN CRIBAR"));
+  d3.select('#ltext1').text(iJS._("VINCHUCA"));
+  d3.select('#ltext2').text(iJS._("REISTENCIA A"));
+  d3.select('#ltext3').text(iJS._("INSECTICIDAS"));
+  d3.select('#fuente').text(iJS._("Fuente: Coalición Global de la Enfermedad de Chagas: Rompiendo el Silencio, una oportunidad para los pacientes de Chagas."));
+
 
   d3.selectAll('#line_0').on("mouseover", function() {
     d3.selectAll('#line_0').classed("active",true)
